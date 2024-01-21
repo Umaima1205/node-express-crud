562680a4ee42bdfec230e9b778526a8d98293973
Umaima Yousuf <112200777+Umaima1205@users.noreply.github.com>
2024-01-21 13:09:12 +0500

    node express crud
    node express crud

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const app = express();
+app.use(express.json());
+
+
+//db url
+const DbUrl = "mongodb+srv://umaimayousuf1205:icL7bHw9wBM5w4qV@cluster0.vkla3pl.mongodb.net/?retryWrites=true&w=majority";
+
+
+//db connected
+mongoose
+.connect(DbUrl)
+.then(() => console.log("Database connected...."))
+.catch((err) => console.log(err));
+
+
+// finding users
+app.get('/' , async(req , res) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    }
+    catch (error){
+        console.log("users"), res.send("Something went wrong....");
+    }
+});
+
+
+//posting users
+app.post('/' , async(req , res) => {
+    try{
+        const users = await User.create(req.body);
+        res.json(users);
+    }
+    catch (error){
+        console.log("users"), res.send("Something went wrong....");
+    }
+});
+
+
+//listening to port
+app.listen(8000, () => {
+    console.log(`Server is listening on ${8000}`);
+});
